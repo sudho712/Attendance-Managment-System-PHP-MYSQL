4a29fb38e26df5457f65df811d3ae37be3508e76
Sudhir Kumar <sudho712@gmail.com>
2024-12-05 22:38:38 +0530

    start a Project Attendance management System

---
+++ database/connect.php
@@ -0,0 +1,28 @@
+<?php
+class Database {
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db = "attendance_db";
+    public $conn;
+
+    // Constructor to establish connection
+    public function __construct() {
+        try {
+            $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->db);
+
+            // Check connection
+            if ($this->conn->connect_error) {
+                throw new Exception("Connection failed: " . $this->conn->connect_error);
+            } else {
+                echo "Connected successfully bbbb";
+            }
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+}
+
+// Create an instance of the Database class
+$db = new Database();
+?>
