b162e2edf1ceef6f492025864a659d43a3b52951
Sudhir Kumar <sudho712@gmail.com>
2024-12-09 19:35:42 +0530

    create attendance table

--- database/createtables.php
+++ database/createtables.php
@@ -4,6 +4,8 @@ require_once $path . "ATTEND/database/connect.php";
 
 $dbo = new Database();
 
+// student table details
+/* 
 $c = "CREATE TABLE student_details (
     id INT AUTO_INCREMENT PRIMARY KEY,
     roll_no VARCHAR(20) UNIQUE,
@@ -16,5 +18,23 @@ try {
     echo "<br> Table student_details created successfully";
 } catch (PDOException $o) {
     echo "<br> Table student_details not created: " . $o->getMessage();
+}
+ */
+$c = "create table attendance_details
+(
+    faculty_id int,
+    course_id int,
+    session_id int,
+    student_id int,
+    on_date date,
+    status VARCHAR(10),
+    primary key (faculty_id, course_id,session_id, student_id, on_date)
+)";
+try{
+    $s=$dbo->conn->prepare($c);
+    $s->execute();
+    echo "<br> attendance table create successfully";
+}catch (PDOException $o){
+    echo "<br> attendance table not create ".$o->getMessage();
 }
 ?>
