ed2c4618de5fa3d989011391944ede601e18ad11
Sudhir Kumar <sudho712@gmail.com>
2024-12-11 00:21:45 +0530

    login page

--- login.php
+++ login.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="./css/login.css">
 </head>
+
 <body>
-    <h1>login</h1>
+
+    <div class="login-form">
+
+        <div class="input-group">
+            <input type="text" id="txt-user" required>
+            <label for="txt-user" id="lbl-user">USERNAME</label>
+        </div>
+
+        <div class="input-group top-margin">
+            <input type="password" id="txt-password" required>
+            <label for="txt-password" id="lbl-password">PASSWORD</label>
+        </div>
+
+        <div class="div-btn-login top-margin">
+            <button class="btn-login">LOGIN</button>
+        </div>
+    </div>
 </body>
+
 </html>
